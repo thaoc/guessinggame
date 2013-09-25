da0c1c22b053bd421aa93fda7299726c4d8fe0a1
Cheng Thao <thaoc@uww.edu>
2013-09-25 18:01:32 -0500

    version 1.1

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ This is a guessing game.
 It will think of a random number between 0 and 100,
 then prompts the user to guess it.  
 
+It let's the user make as many guess as possible.
+
--- guessgame.cpp
+++ guessgame.cpp
@@ -9,16 +9,20 @@ int main(){
   srand(time(NULL));
   int number = rand()%100+1;
   int guess;
+  bool correct = false;
 
   cout << "Thinking of a number (1-100).  Guess?";
-  cin >> guess;
+  while(!correct){
+	  cin >> guess;
 
-  if (number == guess){
-    cout << "You got it." << endl;
-  }  else if (number < guess){
-    cout << "Your guess is high." << endl;
-  } else {
-    cout << "Your guess is low." << endl;
+	  if (number == guess){
+		  cout << "You got it." << endl;
+		  correct = true;
+	  }  else if (number < guess){
+		  cout << "Your guess is high. try again:" << endl;
+	  } else {
+		  cout << "Your guess is low. try again:" << endl;
+	  }
   }
 
 }
